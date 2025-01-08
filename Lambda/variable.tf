fccdc428a877b6282f8e1a7461d8775158a540b5
Yusra310 <yusrarana2001@gmail.com>
2025-01-08 05:42:26 -0800

    Delete Lambda directory

--- Lambda/variable.tf
+++
@@ -1,9 +0,0 @@
-variable "lambda_role_arn" {
-  description = "The ARN of the Lambda execution role"
-  type        = string
-}
-
-# variable "data_table" {
-#   description = "DynamoDB table name for InsertData function"
-#   type        = string
-# }
